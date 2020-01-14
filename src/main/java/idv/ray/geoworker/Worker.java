a4f1fd7bd05ac206cd02a4a9b22a1a8529c94bb3
Ray <as8570865@gmail.com>
2020-01-14 21:54:27 +0800

    connect to master

--- src/main/java/idv/ray/geoworker/Worker.java
+++ src/main/java/idv/ray/geoworker/Worker.java
@@ -3,6 +3,7 @@ package idv.ray.geoworker;
 import java.io.IOException;
 import java.util.Set;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,17 +16,28 @@ public class Worker {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Set<String> geoTypeSet=(Set<String>) context.getBean("geoTypeSet");
 		for(String geoType:geoTypeSet) {
+			JSONObject reqJson = new JSONObject();
+			Task task = new Task(new JSONObject(Connector.getTask(reqJson, geoType)));
+			System.out.println("worker gets task: "+task.toString());
+			
+			Crawler crawler=(Crawler) context.getBean("crawler");
+			Set<String> resultSet=crawler.crawl(task);
+			
+			JSONObject crawledResultJson=new JSONObject();
+			JSONArray urlArr=new JSONArray();
+			JSONObject resultJson=new JSONObject();
+			for(String url:resultSet) {
+				urlArr.put(url);
+			}
+			resultJson.put("value", urlArr);
+			crawledResultJson.put("taskId", task.getId());
+			crawledResultJson.put("type", "task");	
+			crawledResultJson.put("result", resultJson);
+			System.out.print("worker gets next task: "+Connector.getTask(crawledResultJson, geoType));
+			
 			
 		}
-		
-		JSONObject reqJson = new JSONObject();
-		Task task = new Task(new JSONObject(Connector.getTask(reqJson, "test5")));
-		
-		Crawler crawler=(Crawler) context.getBean("crawler");
-		Set<String> resultSet=crawler.crawl(task);
-		for(String link:resultSet)
-			System.out.println(link);
-		
+
 	}
 
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,27 +1,19 @@
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
+import org.json.JSONObject;
+
+import idv.ray.geoworker.Connector;
 
 public class Test {
 
-	private static final String USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-
 	public static void main(String[] args) throws IOException {
-		
-//		Response resp = Jsoup.connect("https://www.geog.illinois.edu/people/shaowen").timeout(10 * 1000).execute();
-//		String contentType = resp.contentType();
-//		
-//		System.out.println("content type: "+contentType);
-		
-		String urlString="https://www.geog.illinois.edu/people/shaowen";
-		System.out.println("url: " + urlString);
-		HttpURLConnection urlConnection = (HttpURLConnection) new URL(urlString).openConnection();
-		System.out.println("open connection finished...");
-		String contentType=urlConnection.getContentType();
-		System.out.println("content type: " + contentType);
+
+		JSONObject crawledResultJson=new JSONObject();
+
+		crawledResultJson.put("taskId", "task");
+		crawledResultJson.put("type", "task");
+		crawledResultJson.put("result", "task");
+		System.out.print("worker gets next task: " + Connector.getTask(crawledResultJson, "sos"));
 	}
 
 }
